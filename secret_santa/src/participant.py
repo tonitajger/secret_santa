db07894cc0250f31e487f0f5d067ba21f50a3ebc
tonitajger <tonyranliu@gmail.com>
2021-11-20 18:00:18 +0100

    Moving logic into methods on Participant and generating solution by only assigning giver

--- secret_santa/src/participant.py
+++ secret_santa/src/participant.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 
 class Participant:
@@ -8,5 +8,38 @@ class Participant:
 		self.giver: Optional[Participant] = None
 		self.receiver: Optional[Participant] = None
 	
+	def is_done(self) -> bool:
+		return self.giver and self.receiver
+	
+	def assign_giver(self, participants: List["Participant"]) -> Optional["Participant"]:
+		if self.giver:
+			raise Exception(f"Error while trying to assign a giver to {self} which already has giver {self.giver}.")
+
+		giver: Optional = self._get_first_possible_giver(participants)
+		if giver is None:
+			return
+		
+		self.giver = giver
+		giver.receiver = self
+		return giver
+
+	
+	def _get_first_possible_giver(self, participants: List["Participant"]) -> Optional["Participant"]:
+
+		for p in participants:
+			if self.group is not None and self.group == p.group:
+				continue
+			if self.giver is not None:
+				continue
+			if p.receiver is not None:
+				continue
+			if p.giver is not None and p.giver == self:
+				continue 
+			return p
+
+	
 	def __str__(self) -> str:
 		return f"{self.name}, {self.group}"
+	
+	def __eq__(self, other: "Participant") -> bool:
+		return self.name == other.name
