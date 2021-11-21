8054b91a5f6ea3cadb8cc201d978973014367d90
tonitajger <tonyranliu@gmail.com>
2021-11-21 01:08:56 +0100

    Setting default run_id to latest output for send_sms

--- secret_santa/src/send_sms.py
+++ secret_santa/src/send_sms.py
@@ -1,6 +1,6 @@
-from os import environ
+from os import environ, walk
 
-from typing import Dict
+from typing import Dict, Optional
 import click
 import clx.xms
 
@@ -9,9 +9,9 @@ from input_output import parse_phone_book
 
 @click.command()
 @click.option("--phone_book_path", help="Path to phone number input file")
-@click.option("--run_id", help="id/timestamp of the generated result. E.g 20211120T231238")
+@click.option("--run_id", default=None, help="id/timestamp of the generated result. E.g 20211120T231238")
 @click.option("--dry_run", "-d", is_flag=True)
-def main(phone_book_path: str, run_id: str, dry_run: bool) -> None:
+def main(phone_book_path: str, run_id: Optional[str], dry_run: bool) -> None:
 
     phones: Dict[str, str] = parse_phone_book(phone_book_path) 
 
@@ -23,6 +23,7 @@ def main(phone_book_path: str, run_id: str, dry_run: bool) -> None:
     batch_params.body = "${message}"
 
     message_dict: Dict[str, str] = {}
+    run_id: str = max(next(walk("output"))[1]) if run_id is None else run_id
     for nr, name in phones.items():
         with open(f"output/{run_id}/files_to_send/{name}.txt", "r") as f:
             message_str = f.read()
@@ -30,8 +31,9 @@ def main(phone_book_path: str, run_id: str, dry_run: bool) -> None:
 
     batch_params.parameters['message'] = message_dict
 
-    print(batch_params.parameters['message'])
-    print(batch_params.recipients)
+    print(f"run_id: {run_id}")
+    print(f"message_dict: {batch_params.parameters['message']}")
+    print(f"recipients: {batch_params.recipients}")
     if dry_run:
         client.create_batch_dry_run(batch_params)
 
@@ -39,6 +41,6 @@ def main(phone_book_path: str, run_id: str, dry_run: bool) -> None:
         run_input: str = input("Are you sure you want to send SMS messsages?! [y/n]")
         if run_input.lower() == "y":
             client.create_batch(batch_params)
-        
+
 if __name__ == '__main__':
     main()
